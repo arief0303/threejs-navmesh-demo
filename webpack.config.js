33ff5fcff45e544c12e86e1e7273a9656e21e4f7
Arief Raihan Syauqie <arief0303@gmail.com>
2024-06-29 02:27:56 +0700

    chore: cleanup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const src = path.resolve(__dirname, 'src');
+const public = path.resolve(__dirname, 'public');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'index.js',
+    path: public
+  },
+  devServer: {
+    static: public,
+  },
+}
